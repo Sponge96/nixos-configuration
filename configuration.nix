465eb48bfe1e31f34dde698973ecf14324adb68c
Sponge <Jackp.bcot@gmail.com>
2024-10-12 20:09:21 +0100

    feat: split hyprland into system_configs

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     ./hardware-configuration.nix
     inputs.home-manager.nixosModules.default
     ./system_configs/steam.nix
+    ./system_configs/hyprland.nix
   ];
 
   # Bootloader.
@@ -55,34 +56,6 @@
   #VIA (KEYBOARD)
   services.udev.packages = with pkgs; [ via ];
 
-  # DISPLAY/HYPRLAND REQUIREMENTS
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  };
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
-    # package = config.boot.kernelPackages.nvidiaPackages.beta;
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "555.58";
-      sha256_64bit = "sha256-bXvcXkg2kQZuCNKRZM5QoTaTjF4l2TtrsKUvyicj5ew=";
-      sha256_aarch64 = lib.fakeSha256;
-      openSha256 = lib.fakeSha256;
-      settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
-      persistencedSha256 = lib.fakeSha256;
-    };
-    nvidiaSettings = true;
-    open = false;
-  };
   # SOUND
   security.rtkit.enable = true;
   services.pipewire = {
