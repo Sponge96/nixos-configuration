6db3939fb3e71fd2f1c07b3550d9daf11e1adb70
Sponge <Jackp.bcot@gmail.com>
2025-12-05 13:32:05 +0000

    enh: updated to 25.11

--- flake.nix
+++ flake.nix
@@ -2,19 +2,19 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
     let
-      profile = "main";
+      profile = "work";
       system = "x86_64-linux";
     in {
       nixosConfigurations.default = nixpkgs.lib.nixosSystem rec {
